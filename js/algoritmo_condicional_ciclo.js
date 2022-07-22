88998d7e58604bab39d5b66e7434ce41124cc50c
delgadie <106634372+delgadie@users.noreply.github.com>
2022-07-21 23:59:33 -0300

    Add files via upload

---
+++ js/algoritmo_condicional_ciclo.js
@@ -0,0 +1,11 @@
+let numeroVeces = parseInt(prompt("Ingrese el número de veces que le gustaría que lo saluden:"));
+
+for(let i=1;i<=20;i++)
+{
+let = resultado = numeroVeces*i;
+if(i==numeroVeces)
+{
+    break;
+}
+alert("Hola");
+}
